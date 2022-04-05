47b4b7de075c05f2c8d1fdf960b1ef0a3d4cf8fd
Tamira <t.gunzburg@gmail.com>
2022-04-05 16:34:22 +0200

    Single route on single weekday with single score of 0 returns insufficient scores msg

--- src/main/java/WhereIsMyTransport/Calculator.java
+++ src/main/java/WhereIsMyTransport/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
     public String calculate_routes(String input_score, String input_routes) {
         String score_string = input_score.substring(input_score.length() - 2);
         String output = "";
-        if (score_string.equals("10")) {
+        if (score_string.equals("10") || score_string.equals(" 0")) {
             output = "Insufficient valid scores provided";
         } else {
             output = "Pantitlán - La Paz Thursday" + score_string;
--- src/test/java/CalculatorTests.java
+++ src/test/java/CalculatorTests.java
@@ -36,4 +36,12 @@ public class CalculatorTests {
         assertEquals(output, calculator.calculate_routes(input_score, input_routes));
     }
 
+    @Test
+    @DisplayName("Single route on single weekday with single score of 0 returns insufficient scores msg")
+    void single_route_single_weekday_single_score_value_0() {
+        String input_score = "2021/11/18;route_4ac7ab76-d938-4b27-93a8-f1b678007dfe 0";
+        String input_routes = "";
+        String output = "Insufficient valid scores provided";
+        assertEquals(output, calculator.calculate_routes(input_score, input_routes));
+    }
 }
